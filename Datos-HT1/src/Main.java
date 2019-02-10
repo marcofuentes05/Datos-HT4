110750e14b8a0b62e1dcc425e3586af2072d9903
Marco Fuentes <marcofuentes1607@gmail.com>
2019-02-10 13:34:36 -0600

    Proyecto Creado

---
+++ Datos-HT1/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Marco Fuentes
+ * Clase Main, contiene el metodo main y se encarga de ejecutar el programa
+ * 
+ */
+public class Main {
+    public static void main (String args []){
+        
+    }
+}
---
+++ Datos-HT1/src/Stack.java
@@ -0,0 +1,39 @@
+/**
+ * La interfaz Stack define el contrato que han de seguir las clases que lo implementen
+ * 
+ * @author fuent
+ * 
+ * 
+ */
+public interface Stack <E>{
+    
+    /**
+     * El metodo push agrega el parametro item a la lista
+     * @param item
+     */
+    public void push (E item);
+    
+    /**
+     * El metodo pop elimina el ultimo objeto de la lista y lo retorna
+     * @return El ultimo elemento de la lista
+     */
+    public E pop();
+   
+    /**
+     * @return El ultimo elemento de la lista
+     */
+    public E peek();
+   
+    /**
+     * El metodo empty permite saber si el stack esta vacio o no
+     * @return True si el stack esta vacio. False de lo contrario
+     */
+    public boolean empty();
+   
+    /**
+     * El metodo size permite saber el tamaño del stack
+     * @return El tamaño del stack
+     */
+    public int size();
+    
+}
